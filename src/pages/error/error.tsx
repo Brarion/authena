1028bb6cc272ab836bf69be1bb87c215992f3069
Николай Прокопенко (brarion) <lyakhov.sr@reliab.tech>
2021-05-04 20:57:20 +0300

    404 styled

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+import styles from './styles.module.scss'
+
 export const Error404 = () => {
-  return <div>404</div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <div>404</div>
+        <div>😥</div>
+      </div>
+    </div>
+  )
 }
